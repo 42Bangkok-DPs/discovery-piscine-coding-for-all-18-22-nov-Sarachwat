b0da577e1fc4d2baf4a0f9e19f7adc36d84b67f6
Sarachwat <Sarach2549@gmail.com>
2024-11-20 17:15:50 +0000

    complate

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Sarach"
+last_name = "Wattanagarunwong" 
+print(first_name)
+print(last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Sarach"
+last_name = "Wattanagarunwong" 
+whole_name = first_name + " " + last_name
+print(whole_name)
